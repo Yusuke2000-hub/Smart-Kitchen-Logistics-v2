2c17d45ae2b82cdfe4e7325c985fea87e8b9cd5c
Yusuke2000 <lemonsawarlove@gmail.com>
2026-04-21 15:22:19 +0900

    Add README.md and update KitchenController

--- src/jp/kitchen/dx/KitchenController.java
+++ src/jp/kitchen/dx/KitchenController.java
@@ -1,4 +1,20 @@
 package jp.kitchen.dx;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class KitchenController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "本日の営業を開始します";
+    }
+
+    @PostMapping("/order")
+    public String order() {
+        return "注文を受け付けました";
+    }
+
 }
