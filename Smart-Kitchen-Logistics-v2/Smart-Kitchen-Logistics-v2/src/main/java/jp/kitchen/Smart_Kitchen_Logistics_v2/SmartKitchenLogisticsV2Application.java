829609568130c693a2fdebdb4032fe692e181cac
Yusuke2000 <lemonsawarlove@gmail.com>
2026-04-29 04:31:10 +0900

    Fix KitchenController package

---
+++ Smart-Kitchen-Logistics-v2/Smart-Kitchen-Logistics-v2/src/main/java/jp/kitchen/Smart_Kitchen_Logistics_v2/SmartKitchenLogisticsV2Application.java
@@ -0,0 +1,13 @@
+package jp.kitchen.Smart_Kitchen_Logistics_v2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartKitchenLogisticsV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartKitchenLogisticsV2Application.class, args);
+	}
+
+}
---
+++ Smart-Kitchen-Logistics-v2/Smart-Kitchen-Logistics-v2/src/test/java/jp/kitchen/Smart_Kitchen_Logistics_v2/SmartKitchenLogisticsV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.kitchen.Smart_Kitchen_Logistics_v2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartKitchenLogisticsV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
